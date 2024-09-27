4cc5ae1f1ab4ccb25f15cbe9deae6c5f07852d52
gstvc <gstv.calca@gmail.com>
2024-09-28 06:02:38 +1000

    feat: added create new activity function w/ backend

--- src/trip-details/activity-modal.tsx
+++ src/trip-details/activity-modal.tsx
@@ -1,15 +1,36 @@
 import { Calendar, Plus, Tag, X } from "lucide-react";
 import { Button } from "../components/button";
+import { FormEvent } from "react";
+import { useParams } from "react-router";
+import { api } from "../lib/axios";
 
 interface createActivityModalProps{
     closeActivityModal: () => void
 }
 
+
+
+
+
 export function CreateActivityModal({closeActivityModal}: createActivityModalProps){
+    const { id } = useParams();
+    async function createAtivity(event: FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const title = data.get('activity_name')?.toString();
+        const occurs_at = data.get('activity_date')?.toString();
+
+        await api.post(`/trips/${id}/activities`, {
+            title,
+            occurs_at
+        })
     
+        window.document.location.reload()
+
+}
     return (
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center"> 
-            <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <form onSubmit={createAtivity} className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-2">
                 <div className="space-y-2">
                 <div className="flex justify-between">
                     <h2 className="text-lg font-semibold">Add new activity</h2>
@@ -19,10 +40,14 @@ export function CreateActivityModal({closeActivityModal}: createActivityModalPro
                 </div>
                     <p className="text-sm text-zinc-400">All participants can see all activities planned</p>
                 </div>
-
+                
                 <div className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
                     <Tag className="size-5 text-zinc-400"/>
-                    <input name="activity_name" type="text" placeholder='Title' className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
+                    <input 
+                        name="activity_name" 
+                        type="text" 
+                        placeholder='Title' 
+                        className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"/>
                 </div>
                 <div className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
                     <Calendar className="size-5 text-zinc-400"/>
@@ -37,7 +62,7 @@ export function CreateActivityModal({closeActivityModal}: createActivityModalPro
                     <Plus className="size-5"/>
                     Add Activiity
                 </Button>
-            </div>
+            </form>
         </div>
     )
 }
