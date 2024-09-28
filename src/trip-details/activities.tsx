beb42625c5999a4deb8ae6cbe2fec48463cc441c
gstvc <gstv.calca@gmail.com>
2024-09-28 19:39:40 +1000

    feat: fetched activities from backend and build the site dinamically

--- src/trip-details/activities.tsx
+++ src/trip-details/activities.tsx
@@ -1,48 +1,59 @@
 import { CheckCircle2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { api } from "../lib/axios";
+import { format } from 'date-fns';
+
+interface ScheduleProps{
+    date: string
+    activities: {
+        id: string
+        title: string
+        occurs_at: string
+    }[]
+}
 
 export function Activities(){
+    const {id} = useParams()
+    const [schedule, setSchedule] = useState<ScheduleProps[]>([])
+
+    useEffect(() => {
+        api.get(`/trips/${id}/activities`).then((response) => {
+            setSchedule(response.data.activities);
+        });
+    }, [id])
+    
+
     return (
         <div className="space-y-8 "> 
-            <div className="space-y-2.5"> {/* Day card div */}
+        {schedule.map((aDaySchedule) => {
+            return(
+                <div className="space-y-2.5"> {/* Day card div */}
 
                 <div className="flex gap-2 items-baseline"> {/* card title div */}
-                    <span className="text-xl text-zinc-300 font-semibold">15th of December</span>
-                    <span className="text-xs text-zinc-500">Sunday</span>
+                    <span className="text-xl text-zinc-300 font-semibold">{format(aDaySchedule.date, "do' of 'MMMM")}</span>
+                    <span className="text-xs text-zinc-500">{format(aDaySchedule.date, 'EEEE')}</span>
                 </div>
-
-                <p className="text-zinc-500">No activities booked for this day.</p>
-
-            </div>
-
-            <div className="space-y-2.5"> {/* Day card div */}
-
-                <div className="flex gap-2 items-baseline"> {/* card title div */}
-                    <span className="text-xl text-zinc-300 font-semibold">16th of December</span>
-                    <span className="text-xs text-zinc-500">Monday</span>
-                </div>
-
-                <div className=""> {/* activity card */}
-                    <div className="flex justify-between bg-zinc-900 p-1 rounded-lg shadow-shape px-4 py-2.5 items-center">
-                        <div className="flex gap-3 text-zinc-100">
-                            <CheckCircle2 className="text-lime-300 size-6"/>
-                            <span>Kart race</span>
+                {aDaySchedule.activities.length === 0 
+                ? <p className="text-zinc-500">No activities booked for this day.</p>
+                : (aDaySchedule.activities.map(activity => {
+                    return (
+                        <div className=""> {/* activity card */}
+                            <div className="flex justify-between bg-zinc-900 p-1 rounded-lg shadow-shape px-4 py-2.5 items-center">
+                                <div className="flex gap-3 text-zinc-100">
+                                    <CheckCircle2 className="text-lime-300 size-6"/>
+                                    <span>{activity.title}</span>
+                                </div>
+                                <span className="text-zinc-400 al-auto">{format(activity.occurs_at, 'HH:mm')}</span>
+                                {}
+                            </div>
                         </div>
-                        <span className="text-zinc-400 al-auto">14:00h</span>
-                    </div>
-                </div>
-
-                <div className=""> {/* activity card */}
-                    <div className="flex justify-between bg-zinc-900 p-1 rounded-lg shadow-shape px-4 py-2.5 items-center">
-                        <div className="flex gap-3 text-zinc-100">
-                            <CheckCircle2 className="text-lime-300 size-6"/>
-                            <span>Kart race</span>
-                        </div>
-                        <span className="text-zinc-400 al-auto">14:00h</span>
-                    </div>
-                </div>
-                
+                    )
+                })
+                )}
             </div>
-        
+            )
+        })}        
         </div>
     )
 }
