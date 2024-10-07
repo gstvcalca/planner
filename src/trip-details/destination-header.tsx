0750dee52c92b6de0bd7ec3d00c449a39a431ebc
gstvc <gstv.calca@gmail.com>
2024-10-08 10:53:27 +1100

    fix: identation

--- src/trip-details/destination-header.tsx
+++ src/trip-details/destination-header.tsx
@@ -22,22 +22,22 @@ export function DestinationHeader({
     return(
         <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
 
-        <div className="flex items-center gap-2">
-            <MapPin className="size-5 text-zinc-400"/> 
-            <span className="text-zinc-100">{tripInfo?.destination}</span>
-        </div>
-        <div className="flex items-center gap-5">
             <div className="flex items-center gap-2">
-                <Calendar className="size-5 text-zinc-400"/> 
-                <span className="text-zinc-100">{displayDate}</span>
+                <MapPin className="size-5 text-zinc-400"/> 
+                <span className="text-zinc-100">{tripInfo?.destination}</span>
             </div>
-            
-            <div className="w-px h-6 bg-zinc-800"/>
-            <Button onClick={openChangeDestinationModal} cstyle="secondary">
-                Change destination/date
-                <Settings2 className="size-5"/>
-            </Button>
+            <div className="flex items-center gap-5">
+                <div className="flex items-center gap-2">
+                    <Calendar className="size-5 text-zinc-400"/> 
+                    <span className="text-zinc-100">{displayDate}</span>
+                </div>
+                
+                <div className="w-px h-6 bg-zinc-800"/>
+                <Button onClick={openChangeDestinationModal} cstyle="secondary">
+                    Change destination/date
+                    <Settings2 className="size-5"/>
+                </Button>
+            </div>
         </div>
-    </div>
     )
 }
